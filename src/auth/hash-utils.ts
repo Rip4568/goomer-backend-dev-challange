8799981dcd58d47930799e80ca012eb6adc856f0
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2024-03-14 14:04:03 -0300

    adiciona arquivos que serão usados no futuro

---
+++ src/auth/hash-utils.ts
@@ -0,0 +1,5 @@
+/* 
+eu ia fazer todo o sistema de encripitação e hash de senhas para login
+porem eu achi desnecessari fazer nesse projeto, e tambem por que acredito por ser um sistema simples não
+vai precisar
+*/
---
+++ src/auth/register.ts
@@ -0,0 +1,7 @@
+/* 
+geralmente eu sempre coloco o arquivo register os metodos
+login
+logout
+signup
+reset_password
+*/
